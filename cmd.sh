134b9bb3448176004d1c495c9b5fcf99bb1182ff
Dale Bewley <dale@bewley.net>
2015-04-25 14:10:03 -0700

    fix tab

--- cmd.sh
+++ cmd.sh
@@ -8,5 +8,5 @@ if [ "$ENV" = "DEV" ]; then
     exec python identidock.py
 else
     echo "Running Production Server"
-	exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py --callable app --stats 0.0.0.0:9191
+    exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py --callable app --stats 0.0.0.0:9191
 fi
