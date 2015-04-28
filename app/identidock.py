070820d6677f700e2363699bacb43c990c04c12f
Dale Bewley <dale@bewley.net>
2015-04-27 20:42:51 -0700

    replace default router with stubbed get_identicon method

--- app/identidock.py
+++ app/identidock.py
@@ -1,9 +1,22 @@
 from flask import Flask
 app = Flask(__name__)
+default_name = 'Dale Bewley'
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!\n'
+def get_identicon():
+	name = default_name
+
+	header = '<html><head><title>Identidock</title></head><body>'
+	body = '''<form method="POST">
+				Hello <input type="text" name="name" value="{}">
+				<input type="submit" value="submit">
+				</form>
+				<p>You look like a:
+				<img src="/monster/monster.png"/>
+				'''.format(name)
+	footer = '</body></html>'
+
+	return header + body + footer
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
