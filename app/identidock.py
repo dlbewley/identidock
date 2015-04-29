2749d03bc9cc7c622cb2334d32f146e4a8f61715
Dale Bewley <dale@bewley.net>
2015-04-28 21:42:20 -0700

    add /monster route to request icon from dnmonster container

--- app/identidock.py
+++ app/identidock.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, Response
+import requests
+
 app = Flask(__name__)
 default_name = 'Dale Bewley'
 
 @app.route('/')
-def get_identicon():
+def mainpage():
 	name = default_name
 
 	header = '<html><head><title>Identidock</title></head><body>'
@@ -17,6 +19,13 @@ def get_identicon():
 	footer = '</body></html>'
 
 	return header + body + footer
+
+@app.route('/monster/<name>')
+def get_identicon(name):
+	r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
+	image = r.content
+
+	return Response(image, mimetype='image/png')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
