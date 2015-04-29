971953b68ab944db8b8ae0c2b4313d046615f121
Dale Bewley <dale@bewley.net>
2015-04-28 21:56:22 -0700

    add support for posting names

--- app/identidock.py
+++ app/identidock.py
@@ -1,21 +1,24 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 import requests
+import urllib.parse
 
 app = Flask(__name__)
 default_name = 'Dale Bewley'
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def mainpage():
 	name = default_name
+	if request.method == 'POST':
+		name = request.form['name']
 
 	header = '<html><head><title>Identidock</title></head><body>'
 	body = '''<form method="POST">
-				Hello <input type="text" name="name" value="{}">
+				Hello <input type="text" name="name" value="{0}">
 				<input type="submit" value="submit">
 				</form>
 				<p>You look like a:
-				<img src="/monster/monster.png"/>
-				'''.format(name)
+				<img src="/monster/{1}"/>
+				'''.format(name, urllib.parse.quote(name))
 	footer = '</body></html>'
 
 	return header + body + footer
